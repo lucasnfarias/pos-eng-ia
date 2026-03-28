73708b47dc876fb398a98f6773a641c7b8250a66
lucasnfarias <lucasnfarias01@gmail.com>
2026-03-28 15:43:38 -0300

    feat: add openrouter api integration example

---
+++ 02-integracao-api-llms/01-smart-model-router-gateway/src/server.ts
@@ -0,0 +1,34 @@
+import Fastify from "fastify";
+import { OpenRouterService } from "./openrouterService.ts";
+
+export const createServer = (openRouterService: OpenRouterService) => {
+  const app = Fastify({ logger: false });
+
+  app.post(
+    "/chat",
+    {
+      schema: {
+        body: {
+          type: "object",
+          required: ["question"],
+          properties: {
+            question: { type: "string", minLength: 5 },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { question } = request.body as { question: string };
+        const response = await openRouterService.generate(question);
+
+        return reply.send(response);
+      } catch (error) {
+        console.error("Error handling /chat request:", error);
+        return reply.code(500);
+      }
+    },
+  );
+
+  return app;
+};
