7f77d57202be988fe22c756dfd73441aa6efa49a
lucasnfarias <lucasnfarias01@gmail.com>
2026-04-19 18:01:23 -0300

    feat: add songs recommender  project

---
+++ 02-integracao-api-llms/04-song-highlights-z/src/config.ts
@@ -0,0 +1,51 @@
+export type ModelConfig = {
+  apiKey: string;
+  httpReferer: string;
+  xTitle: string;
+
+  provider: {
+    sort: {
+      by: string;
+      partition: string;
+    };
+  };
+
+  models: string[];
+  temperature: number;
+
+  memory: {
+    dbUri: string;
+  };
+  maxMessagesToSummary: number;
+};
+
+console.assert(
+  process.env.OPENROUTER_API_KEY,
+  "OPENROUTER_API_KEY is not set in environment variables",
+);
+console.assert(
+  process.env.OPENROUTER_HTTP_REFERER,
+  "OPENROUTER_HTTP_REFERER is not set in environment variables",
+);
+console.assert(
+  process.env.OPENROUTER_X_TITLE,
+  "OPENROUTER_X_TITLE is not set in environment variables",
+);
+
+export const config: ModelConfig = {
+  apiKey: process.env.OPENROUTER_API_KEY!,
+  httpReferer: process.env.OPENROUTER_HTTP_REFERER!,
+  xTitle: process.env.OPENROUTER_X_TITLE!,
+  models: ["arcee-ai/trinity-large-preview:free"],
+  provider: {
+    sort: {
+      by: "throughput", // Route to model with highest throughput (fastest response)
+      partition: "none",
+    },
+  },
+  temperature: 0.7,
+  memory: {
+    dbUri: process.env.MEMORY_DB_URI!,
+  },
+  maxMessagesToSummary: 2,
+};
